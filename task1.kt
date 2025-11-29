9c9e09c28e200e67b927af6046fce547de23a198
noahcoolboy <46800081+noahcoolboy@users.noreply.github.com>
2025-11-29 21:40:35 +0100

    Solve task 1

--- task1.kt
+++ task1.kt
@@ -3,15 +3,16 @@ import kotlin.math.*
 
 
 fun main() {
-   val pt = List(readln().toInt()) {
-       readln().split(" ").map { it.toDouble() }
-   }
-   val ans = pt.map { (x1, y1, h) ->
-       val collide = pt.filter { it[2] > h }
-           .minOf { (x2, y2, h2) ->
-               hypot((x2 - x1), (y2 - y1))
-           }
-       minOf(h, collide).toInt()
-   }
-   println(ans.joinToString("\n"))
+  val pt = List(readln().toInt()) {
+    readln().split(" ").map { it.toDouble() }
+  }
+  val ans = pt.map { (x1, y1, h) ->
+    val collide = pt.filter { it[2] >= h && (it[0] != x1 || it[1] != y1) }
+      .minOfOrNull { (x2, y2, h2) ->
+        hypot((x2 - x1), (y2 - y1))
+      }
+    minOf(h, collide ?: h).toInt()
+  }
+  
+  println(ans.joinToString("\n"))
 }
